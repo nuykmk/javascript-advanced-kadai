5d2f7944caef1f12d95ff555964a9063d78eabc4
nuykmk <yy.uni.yy.yy@gmail.com>
2025-02-14 12:10:08 +0900

    リモートリポジトリから不要なフォルダを削除

--- kadai_020/event.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById("btn");
-
-btn.addEventListener("click", () => {
-  const txt = document.getElementById("text");
-  txt.textContent = "ボタンをクリックしました";
-});
--- kadai_021/event.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("btn");
-const txt = document.getElementById("text");
-
-btn.addEventListener("click", () => {
-  setTimeout(() => {
-    txt.textContent = "ボタンをクリックしました";
-  }, 2000);
-});
